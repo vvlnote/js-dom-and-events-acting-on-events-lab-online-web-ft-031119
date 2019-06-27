d120e5764e75ea5cf55c6019bb18019ae296c036
Joy Lin <vvlnote@hotmail.com>
2019-06-27 21:18:14 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,36 @@ function preventRefreshOnSubmit(){
     })
 }
 
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let eInfo = retrieveEmployeeInformation();
+  let ul = document.getElementsByClassName('employee-list')[0];
+  let li = document.createElement('li');
+  let textnode = document.createTextNode(eInfo);
+  li.appendChild(textnode);
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let f = document.getElementsByTagName('form')[0];
+  f.addEventListener('click', function(e) {
+    addNewElementAsLi();
+    document.querySelector('input').value = "";
+  })
+
+}
+
+function clearEmployeeListOnLinkClick()
+{
+  let alink = document.getElementsByTagName('a')[0];
+  alink.addEventListener('click', function(e) {
+    let ul = document.getElementsByClassName("employee-list")[0];
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+  })
+}
 preventRefreshOnSubmit()
