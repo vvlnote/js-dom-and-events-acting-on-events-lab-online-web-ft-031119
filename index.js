271a57646d5d82044815ce625d73baa10afe1a4d
Joy Lin <vvlnote@hotmail.com>
2019-06-27 20:57:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ function preventRefreshOnSubmit(){
     })
 }
 
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let eInfo = retrieveEmployeeInformation();
+  let ul = document.getElementsByClassName('employee-list')[0];
+  let li = document.createElement('li');
+  let textnode = document.createTextNode(eInfo);
+  li.appendChild(textnode);
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+
+}
+
+function clearEmployeeListOnLinkClick()
+{
+
+}
 preventRefreshOnSubmit()
